148d17ea4d8e4db5d3711fc51e99d5e4c4a7c1f7
wauputr4 <103489788+wauputr4@users.noreply.github.com>
2026-06-13 19:02:32 +0700

    fix: make llms.txt robust for nested list routes

--- src/routes/list/[id]/llms.txt/+server.ts
+++ src/routes/list/[id]/llms.txt/+server.ts
@@ -2,6 +2,6 @@ import { createLlmsResponse } from '$lib/server/llms-content';
 
 export const prerender = true;
 
-export async function GET() {
+export function GET() {
 	return createLlmsResponse();
 }
