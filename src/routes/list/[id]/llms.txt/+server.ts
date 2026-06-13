8c53f62ffbb87aaf8dec49b202d2f81fbb9321ca
wauputr4 <103489788+wauputr4@users.noreply.github.com>
2026-06-13 19:05:38 +0700

    fix: prerender dynamic llms.txt endpoints for list detail routes

--- src/routes/list/[id]/llms.txt/+server.ts
+++ src/routes/list/[id]/llms.txt/+server.ts
@@ -1,6 +1,11 @@
+import { bansosList } from '$lib/data/bansos';
 import { createLlmsResponse } from '$lib/server/llms-content';
 
 export const prerender = true;
+
+export function entries() {
+	return bansosList.map((item) => ({ id: item.id }));
+}
 
 export function GET() {
 	return createLlmsResponse();
